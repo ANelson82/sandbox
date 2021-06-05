dd18011cd634af576b3fdf1bc1e8a34b2c899a3c
Andrew Nelson <andycnelson@gmail.com>
2021-06-04 22:30:35 -0500

    adding files

---
+++ ranged_loops.py
@@ -0,0 +1,11 @@
+#counter = 1
+#while counter < 6:
+#    print(counter)
+#    counter = counter + 1
+
+#for counter in range(1,6):
+#    print(counter)
+
+count_amount = int(input("How far should I count?"))
+for i in range(count_amount):
+    print(i+1)
